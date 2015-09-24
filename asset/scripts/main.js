346b51d5636eb97ae8065cc9569b1edc19d7c3fc
Jassim Abdul Latheef <jassimabdullatheef@gmail.com>
2015-09-24 22:34:48 +0300

    Added Letter

--- asset/scripts/main.js
+++ asset/scripts/main.js
@@ -1,6 +1,12 @@
+// FlipClock
 var clock = $('.timer').FlipClock({
   autoStart : true,
   countdown : true
 });
 clock.setTime(50888);
 clock.start();
+
+// Not Clickable
+$('.notClickable').click(function(e){
+  e.preventDefault();
+})
